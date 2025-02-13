16617f73a997a6da27618193c94b6651fc06a039
prince311206 <princepriyaranjan1234@gamil.com>
2025-02-13 21:56:05 +0530

    PIXOR creation

---
+++ prince.js
@@ -0,0 +1,23 @@
+
+const accountId = 12345 //this value can not be changed
+// double Quotation :-
+let accountGmail = "princepriyaranjan1234@gmail.com" // double Quotation is used for defining a string 
+let accountname = "PRINCE PRIYARANJAN "
+let accountpassword = "311206" // here the numbere are also considered as string
+
+// single Quotation :-
+let name = 'prince' // single Quotation is used to define a single string
+console.log(name);
+
+// backticks :-
+// backticks is used for interpulation of string
+
+let branch = "CSE"
+
+console.log(`my branch is ${branch}`);
+
+// triple Quotation :-
+// javascript doesn't support triple Quote
+// const text = """ this is error """
+
+
